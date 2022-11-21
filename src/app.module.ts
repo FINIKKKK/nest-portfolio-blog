db0f9f32d9c466fdbd68b83282a313beaa2e5bf0
DIGIKRASH <digitallkrash@gmail.com>
2022-11-21 12:50:26 +0600

    resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/entities/user.entity';
+import { PostEntity } from './posts/entities/post.entity';
+import { CommentEntity } from './comments/entities/comment.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1036845297',
+      database: 'portfolio-blog',
+      entities: [UserEntity, PostEntity, CommentEntity],
+      synchronize: true,
+    }),
+    UsersModule,
+    PostsModule,
+    CommentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/posts/entities/post.entity.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class PostEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  text: string;
+
+  @Column()
+  body: string;
+
+  @Column({ default: 0 })
+  views: number;
+
+  @Column({ nullable: true })
+  tags?: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  updateAt: Date;
+}
